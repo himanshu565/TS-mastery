3a4212dd5c65dcf72becdd88bd58599a779efe34
Himanshu <himanshusinghbisht71@gmail.com>
2026-07-13 00:00:02 +0530

    rand file added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-function greet(person: string ) : string {
-    return `hello ${person}`;
+function greet(person: string): string {
+  return `hello ${person}`;
 }
 const username: string = " hello chai pi lo ";
 
 greet(username);
- //
+//
